8f866ad80f976ebf29cf81f4498b15e2b623bc69
skisa31 <skis1031@gmail.com>
2020-01-15 18:47:50 +0900

    複数ファイルの処理のプログラムを追加

---
+++ split_text.py
@@ -0,0 +1,56 @@
+from glob import glob
+
+files = glob('./text/*.txt')
+x = 0
+for file_name in files:
+    x += 1
+    text_file = open(file_name)
+
+    li = []     # 1行ごとに読み込んだテキストの入る配列
+    ele = []    # (P)で区切ったテキストの入る配列
+    txt = []    # (P)で区切ったテキストをjoinしてものが入る配列
+    num = []      # (P)で区切られる配列のインデックスの数字を保持する配列
+
+    for line in text_file:
+        knp = line.strip()
+        e = knp.split("─")
+        li.append(e[0])
+
+    for index, element in enumerate(li):
+        if "(P)" in element or "。" in element:
+            ele.append(element)
+            num.append(index)
+        else:
+            ele.append(element)
+
+    # print(a)
+
+    for i in range(len(num)):
+        if i == 0:
+            txt.append("".join(ele[:num[i]+1]))
+            # print(''.join(ele[:num[i]+1]))
+        elif i == num[-1]:
+            txt.append("".join(ele[num[i]:]))
+            # print(''.join(ele[num[i]:]))
+        else:
+            txt.append("".join(ele[num[i-1]+1:num[i]+1]))
+            # print(''.join(ele[num[i-1]+1:num[i]+1]))
+
+    # print(txt)
+
+    for i in range(len(num)):
+        if "<" in txt[i]:
+            b = txt[i]
+            txt[i] = b.split("<")[0]
+
+    create_file_path = './splited_text/splited_text_' + str(x) + '.txt'
+    with open(create_file_path, 'w', encoding='utf-8') as f:
+
+        for j in range(len(txt)):
+            # print(txt[j])
+            print(txt[j], file=f)
+            if "。" in txt[j]:
+                # print("\n")
+                print('\n', file=f)
+
+    text_file.close
